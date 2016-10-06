ca07555495703505675b463cbc713e0d09d2b274
Alexey <al3x.mak.89@gmail.com>
2016-10-06 22:54:44 +0300

    add file exercise4.py in chapter 6
    solution for exercise 4 in chapter 6

---
+++ chapter6/exercise4.py
@@ -0,0 +1,7 @@
+class Element():
+    def __init__(self, name, symbol, number):
+        self.name = name
+        self.symbol = symbol
+        self.number = number
+
+el1 = Element('Hydrogen', 'H', 1)
