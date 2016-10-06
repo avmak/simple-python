1d957525c4ea5ab10bd05205d5a3b27715d7f9a8
Alexey <al3x.mak.89@gmail.com>
2016-10-06 20:53:08 +0300

    add file exercise2.py and change file exercise1.py in chapter 6
    solution for exercise 2 and change output method for exercise 1 in
    chapter 6

--- chapter6/exercise1.py
+++ chapter6/exercise1.py
@@ -1,6 +1,6 @@
 class Thing():
     pass
 
-Thing               # output: <class '__main__.Thing'>
+print(Thing)               # output: <class '__main__.Thing'>
 example = Thing()
-example             # output: <__main__.Thing object at 0x00000220D710AF98>
+print(example)             # output: <__main__.Thing object at 0x00000220D710AF98>
---
+++ chapter6/exercise2.py
@@ -0,0 +1,4 @@
+class Thing2():
+    letters = 'abc'
+
+Thing2.letters              # output: abc
