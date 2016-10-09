a447ca6071e2f9e450bd4bf83dd19d90cb2f1a01
Alexey <al3x.mak.89@gmail.com>
2016-10-09 23:03:52 +0300

    add file exercise1.py in chapter 7
    solution for exercise 1 in chapter 7

---
+++ chapter7/exercise1.py
@@ -0,0 +1,5 @@
+import unicodedata
+
+mystery = '\U0001f4a9'
+mystery                                                         # output: '\U0001f4a9'
+unicodedata.name(mystery)                                       # output: 'PILE OF POO'
