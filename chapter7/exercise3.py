3a1db6cfb7d4bb7067f554b59ba67a118d285a40
Alexey <al3x.mak.89@gmail.com>
2016-10-10 18:16:05 +0300

    update file exercise3.py in chapter7
    add check variables

--- chapter7/exercise3.py
+++ chapter7/exercise3.py
@@ -4,3 +4,5 @@ mystery = '\U0001f4a9'
 pop_bytes = mystery.encode('utf-8')
 pop_string = pop_bytes.decode('utf-8')
 pop_string                                                          # output: '\U0001f4a9'
+
+mystery == pop_string                                               # output: True
