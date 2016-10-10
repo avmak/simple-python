eacae1f5b8d2501f9f36719787be3df26d6fd78e
Alexey <al3x.mak.89@gmail.com>
2016-10-10 18:06:15 +0300

    add file exercise3.py in chapter7
    solution for exercise 3 in chapter 7

---
+++ chapter7/exercise3.py
@@ -0,0 +1,6 @@
+import unicodedata
+
+mystery = '\U0001f4a9'
+pop_bytes = mystery.encode('utf-8')
+pop_string = pop_bytes.decode('utf-8')
+pop_string                                                          # output: '\U0001f4a9'
