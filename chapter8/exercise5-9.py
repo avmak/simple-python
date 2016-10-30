d14501442e3098dfa1d88232edc9a067d01551df
Alexey Makarov <al3x.mak.89@gmail.com>
2016-10-30 22:25:22 +0300

    add file exercise5-9.py in chapter 8
    solution for exercise 5 in chapter 8

---
+++ chapter8/exercise5-9.py
@@ -0,0 +1,11 @@
+# exercise 5
+
+books = '''title,author,year
+The Weirdstone of Brisingamen,Alan Garner,1960
+Perdido Street Station,China Mieville,2000
+Thud!,Terry Pratchett,2007
+Small Gods,Terry Pratchett,1992
+'''
+
+with open('books.csv', 'wt') as outf:
+    outf.write(books)
