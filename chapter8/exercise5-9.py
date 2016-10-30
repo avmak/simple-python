5dbaebe5d18f4613a63c20b9cae4028344f2da2b
Alexey Makarov <al3x.mak.89@gmail.com>
2016-10-30 22:57:12 +0300

    update file exercise5-9.py in chapter8
    add solution for exercise 8 in chapter 8

--- chapter8/exercise5-9.py
+++ chapter8/exercise5-9.py
@@ -1,5 +1,8 @@
 # exercise 5
 
+import csv
+import sqlite3
+
 books = '''title,author,year
 The Weirdstone of Brisingamen,Alan Garner,1960
 Perdido Street Station,China Mieville,2000
@@ -12,8 +15,6 @@ with open('books.csv', 'wt') as outf:
 
 # exercise 6
 
-import sqlite3
-
 conn = sqlite3.connect('books.db')
 curs = conn.cursor()
 curs.execute('''CREATE TABLE books
@@ -22,11 +23,15 @@ conn.commit()
 
 # exercise 7
 
-import csv
-
 with open('books.csv', 'rt') as inf:
 	rows_db = [(row['title'], row['author'], row['year']) for row in csv.DictReader(inf)]
 
 ins_query = 'INSERT INTO books (title, author, year) VALUES (?, ?, ?)'
 curs.executemany(ins_query, rows_db)
 conn.commit()
+
+# exercise 8
+
+curs.execute('SELECT title FROM books ORDER BY title')
+for row in curs.fetchall():
+	print(row[0])
