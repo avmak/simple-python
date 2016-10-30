af6359aa6b378d4fa968c6ab9af441885a160c60
Alexey Makarov <al3x.mak.89@gmail.com>
2016-10-30 22:44:01 +0300

    update file exercise5-9.py in chapter8
    add solution for exercise 7 in chapter 8

--- chapter8/exercise5-9.py
+++ chapter8/exercise5-9.py
@@ -13,8 +13,20 @@ with open('books.csv', 'wt') as outf:
 # exercise 6
 
 import sqlite3
+
 conn = sqlite3.connect('books.db')
 curs = conn.cursor()
 curs.execute('''CREATE TABLE books
              (title TEXT, author TEXT, year INTEGER)''')
+conn.commit()
+
+# exercise 7
+
+import csv
+
+with open('books.csv', 'rt') as inf:
+	rows_db = [(row['title'], row['author'], row['year']) for row in csv.DictReader(inf)]
+
+ins_query = 'INSERT INTO books (title, author, year) VALUES (?, ?, ?)'
+curs.executemany(ins_query, rows_db)
 conn.commit()
