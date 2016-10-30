9bee6bfaeddccd26ed47a4f5c585e4ea26530f4c
Alexey Makarov <al3x.mak.89@gmail.com>
2016-10-30 23:16:55 +0300

    update file exercise11-12.py in chapter8
    add solution for exercise 12 in chapter8

--- chapter8/exercise11-12.py
+++ chapter8/exercise11-12.py
@@ -5,3 +5,8 @@ import redis
 conn = redis.Redis()
 conn.hmset('test', {'count': '1', 'name': 'Fester Bestertester'})
 conn.hgetall('test')
+
+# exercise 12
+
+conn.hincrby('test', 'count', 1)
+conn.hget('test', 'count')
