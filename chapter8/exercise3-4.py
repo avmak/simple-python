6415c68e1b630fce8e36c8b028c1c91dc6238d22
Alexey Makarov <al3x.mak.89@gmail.com>
2016-10-30 22:19:05 +0300

    add file exercise3-4.py in chapter 8
    solutions for exercise 3 and 4 in chapter 8

---
+++ chapter8/exercise3-4.py
@@ -0,0 +1,18 @@
+# exercise 3
+
+books = '''author,book
+J R R Tolkien,The Hobbit
+Lynne Truss,"Eats, Shoots & Leaves"
+'''
+
+with open('books.csv', 'wt') as outf:
+    outf.write(books)
+
+
+# exercise 4
+
+import csv
+
+with open('books.csv', 'rt') as inf:
+    for row in csv.DictReader(inf):									# output: {'author': 'J R R Tolkien', 'book': 'The Hobbit'}
+		print(row)													# output: {'author': 'Lynne Truss', 'book': 'Eats, Shoots & Leaves'}
