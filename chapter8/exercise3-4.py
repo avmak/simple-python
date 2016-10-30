049033ef074473bddc02e2b61c9edf93028eb63c
Alexey Makarov <al3x.mak.89@gmail.com>
2016-10-30 22:21:41 +0300

    update file exercise3-4.py
    change comment in exercise 4

--- chapter8/exercise3-4.py
+++ chapter8/exercise3-4.py
@@ -15,4 +15,4 @@ import csv
 
 with open('books.csv', 'rt') as inf:
     for row in csv.DictReader(inf):									# output: {'author': 'J R R Tolkien', 'book': 'The Hobbit'}
-		print(row)													# output: {'author': 'Lynne Truss', 'book': 'Eats, Shoots & Leaves'}
+		print(row)										# output: {'author': 'Lynne Truss', 'book': 'Eats, Shoots & Leaves'}
