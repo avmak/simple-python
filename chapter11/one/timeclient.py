6d809f7b3b22209076fb3ad3f7bf339d776cd898
Alexey Makarov <al3x.mak.89@gmail.com>
2016-12-14 22:21:46 +0300

    add file timeclient.py
    create tcp client

---
+++ chapter11/one/timeclient.py
@@ -0,0 +1,16 @@
+import socket
+from encdecode import str_to_bytes, bytes_to_str
+
+strtoser = input('Enter the string: ')
+strtoserb = str_to_bytes(strtoser)
+address = ('localhost', 6789)
+max_size = 1000
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(address)
+client.sendall(strtoserb)
+strfromserb = client.recv(max_size)
+strfromser = bytes_to_str(strfromserb)
+print('Server said: %s' % strfromser)
+print('Client closes session.')
+client.close()
