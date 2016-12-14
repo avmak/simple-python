72c251a3af7665221048008244b37dc4fa33042a
Alexey Makarov <al3x.mak.89@gmail.com>
2016-12-14 22:20:47 +0300

    add file timeserver.py
    create tcp server

---
+++ chapter11/one/timeserver.py
@@ -0,0 +1,29 @@
+import socket
+from datetime import datetime
+from encdecode import str_to_bytes, bytes_to_str
+
+address = ('localhost', 6789)
+max_size = 1000
+
+print('Start the server.')
+print('Waiting for a client to call.')
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(address)
+server.listen(5)
+client, addr = server.accept()
+strfromclib = client.recv(max_size)
+strfromcli = bytes_to_str(strfromclib)
+print('Client said: %s.' % strfromcli)
+if strfromcli == 'time':
+    today = datetime.now()
+    todayiso = today.isoformat()
+    todayisob = str_to_bytes(todayiso)
+    client.sendall(todayisob)
+else:
+    strelse = 'What?'
+    strelseb = str_to_bytes(strelse)
+    client.sendall(strelseb)
+print('Server closes session.')
+client.close()
+server.close()
